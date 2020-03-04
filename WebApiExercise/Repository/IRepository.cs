985457a38ccdcc5f385279c4ad0704b1dc2c33c9
YuChia <h42279@gmail.com>
2020-03-04 22:33:25 +0800

    Add Repository

---
+++ WebApiExercise/Repository/IRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SkillTree_MVC_HW.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Lookups all.
+        /// </summary>
+        /// <returns>IQueryable&lt;T&gt;.</returns>
+        IQueryable<T> LookupAll();
+
+        /// <summary>
+        /// 搜尋
+        /// </summary>
+        /// <returns></returns>
+        IQueryable<T> Query(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// 取得單一 entity
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        T GetSingle(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// 新增一個entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Create(T entity);
+
+        /// <summary>
+        /// 刪除單一entity
+        /// </summary>
+        /// <param name="entity"></param>
+        void Remove(T entity);
+    }
+}
---
+++ WebApiExercise/Repository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace SkillTree_MVC_HW.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private DbSet<T> _objectSet;
+        public Repository(DbContext dbContext)
+        {
+            _objectSet = dbContext.Set<T>();
+        }
+
+        public IQueryable<T> LookupAll()
+        {
+            return _objectSet;
+        }
+
+        public IQueryable<T> Query(Expression<Func<T, bool>> filter)
+        {
+            return _objectSet.Where(filter);
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> filter)
+        {
+            return _objectSet.SingleOrDefault(filter);
+        }
+
+        public void Create(T entity)
+        {
+            _objectSet.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _objectSet.Remove(entity);
+        }
+    }
+}
